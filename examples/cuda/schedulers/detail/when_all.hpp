fb124c34504594cea3b9d0587dbabb7818c0bf88
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2022-09-07 15:43:44 +0400

    Trying to take when all out of p2300 ns

--- examples/cuda/schedulers/detail/when_all.hpp
+++ examples/cuda/schedulers/detail/when_all.hpp
@@ -22,5 +22,293 @@
 
 namespace example::cuda::stream::when_all {
 
+enum state_t { started, error, stopped };
+
+struct on_stop_requested {
+  std::in_place_stop_source& stop_source_;
+  void operator()() noexcept {
+    stop_source_.request_stop();
+  }
+};
+
+template <class Env>
+  using env_t =
+    std::execution::make_env_t<Env, std::execution::with_t<std::execution::get_stop_token_t, std::in_place_stop_token>>;
+
+template <class...>
+  using swallow_values = std::execution::completion_signatures<>;
+
+template <class Env, class... Senders>
+  struct traits {
+    using __t = std::execution::dependent_completion_signatures<Env>;
+  };
+
+template <class Env, class... Senders>
+    requires ((std::__v<std::execution::__count_of<std::execution::set_value_t, Senders, Env>> <= 1) &&...)
+  struct traits<Env, Senders...> {
+    using non_values =
+      std::execution::__concat_completion_signatures_t<
+        std::execution::completion_signatures<
+          std::execution::set_error_t(std::exception_ptr),
+          std::execution::set_stopped_t()>,
+        std::execution::make_completion_signatures<
+          Senders,
+          Env,
+          std::execution::completion_signatures<>,
+          swallow_values>...>;
+    using values =
+      std::__minvoke<
+        std::__concat<std::__qf<std::execution::set_value_t>>,
+        std::execution::__value_types_of_t<
+          Senders,
+          Env,
+          std::__q<std::__types>,
+          std::__single_or<std::__types<>>>...>;
+    using __t =
+      std::__if_c<
+        (std::execution::__sends<std::execution::set_value_t, Senders, Env> &&...),
+        std::__minvoke2<
+          std::__push_back<std::__q<std::execution::completion_signatures>>, non_values, values>,
+        non_values>;
+  };
+
+template <class... SenderIds>
+  struct sender_t {
+    template <class... Sndrs>
+      explicit sender_t(Sndrs&&... __sndrs)
+        : sndrs_((Sndrs&&) __sndrs...)
+      {}
+
+   private:
+    template <class CvrefEnv>
+      using completion_sigs =
+        std::__t<traits<
+          env_t<std::remove_cvref_t<CvrefEnv>>,
+          std::__member_t<CvrefEnv, std::__t<SenderIds>>...>>;
+
+    template <class Traits>
+      using sends_values =
+        std::__bool<std::__v<typename Traits::template
+          __gather_sigs<std::execution::set_value_t, std::__mconst<int>, std::__mcount>> != 0>;
+
+    template <class CvrefReceiverId>
+      struct operation_t;
+
+    template <class CvrefReceiverId, std::size_t Index>
+      struct receiver_t : std::execution::receiver_adaptor<receiver_t<CvrefReceiverId, Index>>, example::cuda::stream::receiver_base_t {
+        using WhenAll = std::__member_t<CvrefReceiverId, sender_t>;
+        using Receiver = std::__t<std::decay_t<CvrefReceiverId>>;
+        using Traits =
+          completion_sigs<
+            std::__member_t<CvrefReceiverId, std::execution::env_of_t<Receiver>>>;
+
+        Receiver&& base() && noexcept {
+          return (Receiver&&) op_state_->recvr_;
+        }
+        const Receiver& base() const & noexcept {
+          return op_state_->recvr_;
+        }
+        template <class Error>
+          void set_error(Error&& err, state_t expected) noexcept {
+            // TODO: _What memory orderings are actually needed here?
+            if (op_state_->state_.compare_exchange_strong(expected, error)) {
+              op_state_->stop_source_.request_stop();
+              // We won the race, free to write the error into the operation
+              // state without worry.
+              try {
+                op_state_->errors_.template emplace<std::decay_t<Error>>((Error&&) err);
+              } catch(...) {
+                op_state_->errors_.template emplace<std::exception_ptr>(std::current_exception());
+              }
+            }
+            op_state_->arrive();
+          }
+        template <class... Values>
+          void set_value(Values&&... vals) && noexcept {
+            if constexpr (sends_values<Traits>::value) {
+              // We only need to bother recording the completion values
+              // if we're not already in the "error" or "stopped" state.
+              if (op_state_->state_ == started) {
+                try {
+                  std::get<Index>(op_state_->values_).emplace(
+                      (Values&&) vals...);
+                } catch(...) {
+                  set_error(std::current_exception(), started);
+                }
+              }
+            }
+            op_state_->arrive();
+          }
+        template <class Error>
+            requires std::tag_invocable<std::execution::set_error_t, Receiver, Error>
+          void set_error(Error&& err) && noexcept {
+            set_error((Error&&) err, started);
+          }
+        void set_stopped() && noexcept {
+          state_t expected = started;
+          // Transition to the "stopped" state if and only if we're in the
+          // "started" state. (If this fails, it's because we're in an
+          // error state, which trumps cancellation.)
+          if (op_state_->state_.compare_exchange_strong(expected, stopped)) {
+            op_state_->stop_source_.request_stop();
+          }
+          op_state_->arrive();
+        }
+        auto get_env() const
+          -> std::execution::make_env_t<std::execution::env_of_t<Receiver>, std::execution::with_t<std::execution::get_stop_token_t, std::in_place_stop_token>> {
+          return std::execution::make_env(
+            std::execution::get_env(base()),
+            std::execution::with(std::execution::get_stop_token, op_state_->stop_source_.get_token()));
+        }
+        operation_t<CvrefReceiverId>* op_state_;
+      };
+
+    template <class CvrefReceiverId>
+      struct operation_t {
+        using WhenAll = std::__member_t<CvrefReceiverId, sender_t>;
+        using Receiver = std::__t<std::decay_t<CvrefReceiverId>>;
+        using Env = std::execution::env_of_t<Receiver>;
+        using CvrefEnv = std::__member_t<CvrefReceiverId, Env>;
+        using Traits = completion_sigs<CvrefEnv>;
+
+        template <class Sender, std::size_t Index>
+          using child_op_state =
+            std::execution::connect_result_t<
+              std::__member_t<WhenAll, Sender>,
+              receiver_t<CvrefReceiverId, Index>>;
+
+        using Indices = std::index_sequence_for<SenderIds...>;
+
+        template <size_t... Is>
+          static auto connect_children_(std::index_sequence<Is...>)
+            -> std::tuple<child_op_state<std::__t<SenderIds>, Is>...>;
+
+        using child_op_states_tuple_t =
+            decltype((connect_children_)(Indices{}));
+
+        void arrive() noexcept {
+          if (0 == --count_) {
+            complete();
+          }
+        }
+
+        void complete() noexcept {
+          // Stop callback is no longer needed. Destroy it.
+          on_stop_.reset();
+          // All child operations have completed and arrived at the barrier.
+          switch(state_.load(std::memory_order_relaxed)) {
+          case started:
+            if constexpr (sends_values<Traits>::value) {
+              // TODO stream sync instead?
+              // TODO specialize when_all only when all senders are stream ones, then wait on them
+              cudaDeviceSynchronize();
+
+              // All child operations completed successfully:
+              std::apply(
+                [this](auto&... opt_vals) -> void {
+                  std::apply(
+                    [this](auto&... all_vals) -> void {
+                      try {
+                      std::execution::set_value(
+                            (Receiver&&) recvr_, std::move(all_vals)...);
+                      } catch(...) {
+                      std::execution::set_error(
+                            (Receiver&&) recvr_, std::current_exception());
+                      }
+                    },
+                    std::tuple_cat(
+                      std::apply(
+                        [](auto&... vals) { return std::tie(vals...); },
+                        *opt_vals
+                      )...
+                    )
+                  );
+                },
+                values_
+              );
+            }
+            break;
+          case error:
+            std::visit([this](auto& err) noexcept {
+                std::execution::set_error((Receiver&&) recvr_, std::move(err));
+            }, errors_);
+            break;
+          case stopped:
+            std::execution::set_stopped((Receiver&&) recvr_);
+            break;
+          default:
+            ;
+          }
+        }
+
+        template <size_t... Is>
+          operation_t(WhenAll&& when_all, Receiver rcvr, std::index_sequence<Is...>)
+            : child_states_{
+              std::execution::__conv{[&when_all, this]() {
+                  return std::execution::connect(
+                      std::get<Is>(((WhenAll&&) when_all).sndrs_),
+                      receiver_t<CvrefReceiverId, Is>{{}, {}, this});
+                }}...
+              }
+            , recvr_((Receiver&&) rcvr)
+          {}
+        operation_t(WhenAll&& when_all, Receiver rcvr)
+          : operation_t((WhenAll&&) when_all, (Receiver&&) rcvr, Indices{})
+        {}
+        _P2300_IMMOVABLE(operation_t);
+
+        friend void tag_invoke(std::execution::start_t, operation_t& self) noexcept {
+          // register stop callback:
+          self.on_stop_.emplace(
+              std::execution::get_stop_token(std::execution::get_env(self.recvr_)),
+              on_stop_requested{self.stop_source_});
+          if (self.stop_source_.stop_requested()) {
+            // Stop has already been requested. Don't bother starting
+            // the child operations.
+            std::execution::set_stopped((Receiver&&) self.recvr_);
+          } else {
+            apply([](auto&&... __child_ops) noexcept -> void {
+              (std::execution::start(__child_ops), ...);
+            }, self.child_states_);
+          }
+        }
+
+        // tuple<optional<tuple<Vs1...>>, optional<tuple<Vs2...>>, ...>
+        using child_values_tuple_t =
+          std::__if<
+            sends_values<Traits>,
+            std::__minvoke<
+              std::__q<std::tuple>,
+              std::execution::__value_types_of_t<
+                std::__t<SenderIds>,
+                env_t<Env>,
+                std::__mcompose<std::__q1<std::optional>, std::__q<std::execution::__decayed_tuple>>,
+                std::__single_or<void>>...>,
+            std::__>;
+
+        child_op_states_tuple_t child_states_;
+        Receiver recvr_;
+        std::atomic<std::size_t> count_{sizeof...(SenderIds)};
+        // Could be non-atomic here and atomic_ref everywhere except __completion_fn
+        std::atomic<state_t> state_{started};
+        std::execution::error_types_of_t<sender_t, env_t<Env>, std::execution::__variant> errors_{};
+        [[no_unique_address]] child_values_tuple_t values_{};
+        std::in_place_stop_source stop_source_{};
+        std::optional<typename std::execution::stop_token_of_t<std::execution::env_of_t<Receiver>&>::template
+            callback_type<on_stop_requested>> on_stop_{};
+      };
+
+    template <std::__decays_to<sender_t> Self, std::execution::receiver Receiver>
+      friend auto tag_invoke(std::execution::connect_t, Self&& self, Receiver&& rcvr)
+        -> operation_t<std::__member_t<Self, std::__x<std::decay_t<Receiver>>>> {
+        return {(Self&&) self, (Receiver&&) rcvr};
+      }
+
+    template <std::__decays_to<sender_t> Self, class Env>
+      friend auto tag_invoke(std::execution::get_completion_signatures_t, Self&&, Env)
+        -> completion_sigs<std::__member_t<Self, Env>>;
+
+    std::tuple<std::__t<SenderIds>...> sndrs_;
+  };
 }
 
