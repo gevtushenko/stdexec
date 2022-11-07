1e1ea89bad23f0275d5785d3f74b080666563967
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2022-11-07 17:40:52 +0400

    Coop bulk

--- examples/coop.cpp
+++ examples/coop.cpp
@@ -202,6 +202,115 @@ namespace coop {
       }
     };
 
+  namespace bulk {
+    template <class Shape, class Fun, class ReceiverId>
+      struct receiver_t {
+        using Receiver = stdexec::__t<ReceiverId>;
+        using Env = stdexec::env_of_t<Receiver>;
+
+        context_state state_;
+        Shape shape_;
+        Fun fun_;
+        Receiver receiver_;
+
+        static std::pair<Shape, Shape>
+        even_share(Shape n, std::uint32_t rank, std::uint32_t size) noexcept {
+          const auto avg_per_thread = n / size;
+          const auto n_big_share = avg_per_thread + 1;
+          const auto big_shares = n % size;
+          const auto is_big_share = rank < big_shares;
+          const auto begin = is_big_share ? n_big_share * rank
+                                          : n_big_share * big_shares +
+                                              (rank - big_shares) * avg_per_thread;
+          const auto end = begin + (is_big_share ? n_big_share : avg_per_thread);
+
+          return std::make_pair(begin, end);
+        }
+
+        template <stdexec::__one_of<stdexec::set_error_t,
+                                    stdexec::set_stopped_t> Tag,
+                  class... As>
+        friend void tag_invoke(Tag tag, receiver_t&& self, As&&... as) noexcept {
+          if (self.state_.thread_id_ == 0) {
+            tag(std::move(self.receiver_), (As&&)as...);
+          } else {
+            // TODO
+            // aux_channel(std::move(self.receiver_), (As&&)as...);
+          }
+        }
+
+        template <class... As>
+        friend void tag_invoke(stdexec::set_value_t, receiver_t&& self, As&&... as) noexcept {
+          // Meet other threds there
+          aux_channel(std::move(self), (As&)as...);
+        }
+
+        template <class... As>
+        friend void tag_invoke(aux_channel_t, receiver_t&& self, As&&... as) noexcept {
+          self.state_.barrier_->arrive_and_wait();
+
+          auto [begin, end] = 
+            receiver_t::even_share(self.shape_, self.state_.thread_id_, self.state_.num_threads_);
+
+          for (Shape i = begin; i < end; ++i) {
+            self.fun_(i, as...);
+          }
+
+          if (self.state_.thread_id_ == 0) {
+            stdexec::set_value(std::move(self.receiver_), (As&&)as...);
+          } else {
+            aux_channel(std::move(self.receiver_), (As&&)as...);
+          }
+        }
+
+        friend stdexec::env_of_t<Receiver>
+        tag_invoke(stdexec::get_env_t, const receiver_t& self) {
+          return stdexec::get_env(self.receiver_);
+        }
+      };
+  }
+
+  template <class SenderId, class Shape, class Fun>
+    struct bulk_sender_t {
+      using Sender = stdexec::__t<SenderId>;
+
+      context_state state_;
+      Sender sndr_;
+      Shape shape_;
+      Fun fun_;
+
+      template <class Receiver>
+        using receiver_t = bulk::receiver_t<Shape, Fun, stdexec::__id<std::decay_t<Receiver>>>;
+
+      template <stdexec::__decays_to<bulk_sender_t> Self, stdexec::receiver Receiver>
+          requires stdexec::sender_to<stdexec::__member_t<Self, Sender>, Receiver>
+        friend auto tag_invoke(stdexec::connect_t, Self&& self, Receiver&& rcvr)
+          -> stdexec::connect_result_t<stdexec::__member_t<Self, Sender>, receiver_t<Receiver>> {
+            return stdexec::connect(
+                ((Self&&)self).sndr_, 
+                receiver_t<Receiver>{
+                  self.state_, 
+                  self.shape_,
+                  self.fun_,
+                  (Receiver&&)rcvr});
+        }
+
+      template <stdexec::tag_category<stdexec::forwarding_sender_query> _Tag, class... _As>
+          requires stdexec::__callable<_Tag, const Sender&, _As...>
+        friend auto tag_invoke(_Tag __tag, const bulk_sender_t& __self, _As&&... __as)
+          noexcept(stdexec::__nothrow_callable<_Tag, const Sender&, _As...>)
+          -> stdexec::__call_result_if_t<stdexec::tag_category<_Tag, stdexec::forwarding_sender_query>, _Tag, const Sender&, _As...> {
+          return ((_Tag&&) __tag)(__self.sndr_, (_As&&) __as...);
+        }
+
+      template <stdexec::__decays_to<bulk_sender_t> _Self, class _Env>
+        friend auto tag_invoke(stdexec::get_completion_signatures_t, _Self&&, _Env) ->
+          stdexec::make_completion_signatures<
+            stdexec::__member_t<_Self, Sender>,
+            _Env,
+            stdexec::completion_signatures<aux_channel_t()>>;
+    };
+
   struct inline_scheduler {
     context_state state_;
 
@@ -261,6 +370,16 @@ namespace coop {
         return schedule_from_sender_th<S>(sch.state_, (S&&) sndr);
       }
 
+    template <stdexec::sender Sender, class Shape, class Fun>
+      using bulk_sender_th = 
+        bulk_sender_t<stdexec::__id<std::decay_t<Sender>>, Shape, Fun>;
+
+    template <stdexec::sender S, std::integral Shape, class Fn>
+      friend bulk_sender_th<S, Shape, Fn>
+      tag_invoke(stdexec::bulk_t, const inline_scheduler& sch, S&& sndr, Shape shape, Fn fun) noexcept {
+        return bulk_sender_th<S, Shape, Fn>{sch.state_, (S&&) sndr, shape, fun};
+      }
+
     friend stdexec::forward_progress_guarantee tag_invoke(
         stdexec::get_forward_progress_guarantee_t,
         const inline_scheduler&) noexcept {
@@ -290,10 +409,8 @@ namespace coop {
 struct empty_env {};
 
 struct recv {
-  // friend void tag_invoke(coop::set_value_t, recv&&) noexcept {}
-  friend void tag_invoke(stdexec::set_value_t, recv&&) noexcept {}
-  friend void tag_invoke(stdexec::set_stopped_t, recv&&) noexcept {}
-  friend void tag_invoke(stdexec::set_error_t, recv&&, std::exception_ptr) noexcept {}
+  template <class T, class... Args>
+    friend void tag_invoke(T, recv&&, Args&&...) noexcept {}
   friend empty_env tag_invoke(stdexec::get_env_t, const recv&) noexcept { return {}; }
 };
 
@@ -305,18 +422,27 @@ int main() {
   for (std::size_t thread_id = 0; thread_id < num_threads; thread_id++) {
     threads[thread_id] = std::thread([thread_id, &ctx] {
       auto sch = ctx.get_scheduler(thread_id);
-      auto snd = stdexec::just() 
+      auto snd = stdexec::just()
                | exec::on(exec::inline_scheduler{},
                           stdexec::then([] { 
-                            std::printf("inline\n"); 
+                            std::printf("inline::then\n"); 
+                          })
+                        | stdexec::bulk(2, [](int id) {
+                            std::printf("inline::bulk(%d)\n", id);
                           }))
                | exec::on(sch,
                           stdexec::then([] { 
                             std::printf("coop\n"); 
+                          })
+                        | stdexec::bulk(2, [](int id) {
+                            std::printf("coop::bulk(%d)\n", id);
                           }))
                | exec::on(exec::inline_scheduler{},
                           stdexec::then([] { 
                             std::printf("inline\n"); 
+                          })
+                        | stdexec::bulk(2, [](int id) {
+                            std::printf("inline::bulk(%d)\n", id);
                           }));
       stdexec::sync_wait(std::move(snd));
     });
